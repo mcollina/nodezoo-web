21f607a921e08a2ebf3fd1e37f2388033e10ec0e
Matteo Collina <hello@matteocollina.com>
2015-01-15 12:43:09 +0000

    added seneca-msgstat, bumped seneca to 0.6.0 git.

--- seneca-init.js
+++ seneca-init.js
@@ -3,14 +3,20 @@
 var seneca = require('seneca');
 var api = seneca();
 
+var beanstakIP = process.env.BEANSTALK_IP || 'localhost';
+var influxIP = process.env.INFLUX_IP || 'localhost';
+
 
 if( 'dev' !== api.options().env ) {
   api
     .use('./lib/api.js')
     .use('beanstalk-transport')
-    .client({host: process.env.BEANSTALK_IP, type: 'beanstalk', port:1130, pin:'role:info,cmd:*'})
-    .client({host: process.env.BEANSTALK_IP, type: 'beanstalk', port:1130, pin:'role:search,cmd:*'})
-    .listen({host: process.env.BEANSTALK_IP, type: 'beanstalk', port:1130});
+    .use('data-editor')
+    .use('msgstats', { influxOpts: { host: influxIP, database: 'stats', seriesName: 'actions', username: 'root', password: 'root', port: '8086' } })
+    .use('admin', {local:true})
+    .client({host: beanstakIP, type: 'beanstalk', port:1130, pin:'role:info,cmd:*'})
+    .client({host: beanstakIP, type: 'beanstalk', port:1130, pin:'role:search,cmd:*'})
+    .listen({host: beanstakIP, type: 'beanstalk', port:1130});
 }
 else {
   api
